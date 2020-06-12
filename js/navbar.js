2fa8f87f91a781498345a8c7444b4b1132acd07b
Aryan Sharma <aryabsharma11223@gmail.com>
2020-06-12 10:48:50 +0530

    Navbar Animation Changed

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,26 @@
-$(document).ready(function () {
-  $(".navBtn a").click(function () {
-    $(".overlay").fadeToggle(200);
-    $(this).toggleClass("btn-open").toggleClass("btn-close");
+// $(document).ready(function () {
+//   $(".navBtn a").click(function () {
+//     $(".overlay").fadeToggle(200);
+//     $(this).toggleClass("btn-open").toggleClass("btn-close");
+//   });
+// });
+// $(".overlay").on("click", function () {
+//   $(".overlay").fadeToggle(200);
+//   $(".navBtn a").toggleClass("btn-open").toggleClass("btn-close");
+//   open = false;
+// });
+
+
+//Menu Button
+
+$(document).ready(function() {
+  $(".navBtn a").click(function() {
+    $(".overlay").show(1000);
   });
 });
-$(".overlay").on("click", function () {
-  $(".overlay").fadeToggle(200);
-  $(".navBtn a").toggleClass("btn-open").toggleClass("btn-close");
-  open = false;
+
+$(document).ready(function() {
+  $(".overlay").click(function() {
+    $(".overlay").hide(1000);
+  });
 });
