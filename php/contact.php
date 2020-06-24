2ef11c33e5f09ad3e3e69f2533d8e53e1212c4ec
Aryan Sharma <aryabsharma11223@gmail.com>
2020-06-24 09:45:21 +0530

    Improve php file

--- php/contact.php
+++ php/contact.php
@@ -7,14 +7,14 @@
 
         $to = "thecodefire.arul@gmail.com";
 
-        $subject = "Form Submission";
+        $subject = "theCodeFire Form Submission";
 
-        $msg = "Name: ".$name."\n"."Email Address: "."\n".$email."\n"."Wrote the Following: "."\n\n".$message;
+        $msg = "Name: "."\n".$name."\n"."Email Address: "."\n".$email."\n"."Wrote the Following: "."\n\n".$message;
 
         $header = "From: ".$email;
 
         if(mail($to, $subject, $msg, $headers)){
-            echo "<script>alert('Hey '.$name.', Yoru Message have been send successfully. We will contact you shortly')</script>";
+            echo "<script>alert('Hey '.$name.', Yoru Message have been send successfully. We will contact you shortly');</script>";
 
             echo "<script>window.open('contact.html', '_self');</script>";
         }
